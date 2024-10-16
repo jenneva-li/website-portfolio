dedd00ad6e7b5572371e8879d3465d1e3a87a6f7
jenneva-li <164729882+Jenneva-Li@users.noreply.github.com>
2024-10-15 19:10:19 -0700

    update

--- projects/script.js
+++ projects/script.js
@@ -40,14 +40,18 @@ function getProjects() {
 }
 
 
+
 function showProjects(projects) {
+    
     let projectsContainer = document.querySelector(".work .box-container");
     let projectsHTML = "";
     projects.forEach(project => {
+        console.log(`/assets/images/projects/${project.image}.jpg`);  // Check the path in the console
+        
         projectsHTML += `
         <div class="grid-item ${project.category}">
         <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <img draggable="false" src="./assets/images/projects/${project.image}.jpg" alt="project" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
